2dcebdacade39a8a0facdcc1ca1652a15a47b380
Tim Sullivan <timothyj.sullivan1@gmail.com>
2025-11-25 02:14:12 +0000

    fixed terraform

--- infra/catalog/terraform_modules/bookmarks_web_apps/versions.tf
+++ infra/catalog/terraform_modules/bookmarks_web_apps/versions.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80"
+      version = "~> 4.40"
     }
     random = {
       source  = "hashicorp/random"
